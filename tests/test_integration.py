a3a3610f513f4ed31a1b40850d5451a72e730d2b
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-12-02 15:18:49 +0000

    Added an integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,9 @@
+from sunflower.maker import make_sunflower
+
+def test_end_to_end():
+    sunflower = make_sunflower(rings=5)
+    with open("tests/data/sunflower-expected.svg", "r") as filein:
+        expected_sunflower = filein.read()
+
+    assert sunflower == expected_sunflower
+
