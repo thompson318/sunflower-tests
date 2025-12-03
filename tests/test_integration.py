be96968f14d76678625b463c06f4ee68437c85ad
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-12-03 09:57:45 +0000

    Update test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,7 @@
 from sunflower.maker import make_sunflower
 
 def test_end_to_end():
-    sunflower = make_sunflower(rings=5)
+    sunflower = make_sunflower(rank=0)
     with open("tests/data/sunflower-expected.svg", "r") as filein:
         expected_sunflower = filein.read()
 
