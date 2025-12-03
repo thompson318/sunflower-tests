01fa0e4c7a74d97269f1d75b8d562e07d56a3547
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-12-03 11:57:06 +0000

    Added unit test and working code

--- sunflower/fibonacci.py
+++ sunflower/fibonacci.py
@@ -3,8 +3,10 @@ import typing
 
 def fibonacci(rank):
     rank = int(rank)
-    if not rank in range(6):
-        raise ValueError("fibonacci only works for positive values up to 5")
-
-    values = [0, 1, 1, 2, 3, 5]
+    
+    values = [0,1]
+    i = 2
+    while rank > 1 and i <= rank:
+        values.append(values[i-1] + values[i-2])
+        i = i + 1
     return values[rank]
